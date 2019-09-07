97db11c6f996554e75364df95e108fe279d3a818
Demyx <info@demyx.sh>
2019-09-07 02:04:16 -0700

    Fixed domain replacement

--- demyx-entrypoint.sh
+++ demyx-entrypoint.sh
@@ -18,8 +18,8 @@ fi
 
 # Domain replacement
 if [[ -n "$WORDPRESS_DOMAIN" ]]; then
-	sed -i "s|demyx.|$WORDPRESS_DOMAIN.|g" /etc/nginx/nginx.conf
-	sed -i "s|demyx.error|$WORDPRESS_DOMAIN.error|g" /etc/php7/php-fpm.d/www.conf
+	sed -i "s|/var/log/demyx/demyx|/var/log/demyx/$WORDPRESS_DOMAIN|g" /etc/nginx/nginx.conf
+	sed -i "s|/var/log/demyx/demyx|/var/log/demyx/$WORDPRESS_DOMAIN|g" /etc/php7/php-fpm.d/www.conf
 fi
 
 # Cloudflare check
